95f8a15d71d5edfe4ae884d285363868f8b3a63a
wenyingxin <wenyingxin2010@sina.com>
2019-01-03 17:09:00 +0800

    2019first commit

---
+++ 2018/Day1/src/main/java/com/example/springcloud/day1/Day1Application.java
@@ -0,0 +1,14 @@
+package com.example.springcloud.day1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day1Application.class, args);
+	}
+
+}
+
